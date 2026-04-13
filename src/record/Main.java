1b6f4d25cde11d0814e7c54fc79651c92b97b035
shiivu7 <shokdarshivum@gmail.com>
2026-04-13 21:36:08 +0530

    initial Commit-Student Result System

---
+++ src/record/Main.java
@@ -0,0 +1,16 @@
+package record;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s=new Student(1,"Shiivu",19);
+		Result r=new Result(1,"maths",75);
+		
+		
+		System.out.println(s);
+		System.out.println(r);
+
+	}
+
+}
