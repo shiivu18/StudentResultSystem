7cb5468a77ba260cef66e5c9205037934ad5c088
shiivu7 <shokdarshivum@gmail.com>
2026-04-18 00:23:56 +0530

    day 6 of student management System

--- src/record/Main.java
+++ src/record/Main.java
@@ -1,36 +1,72 @@
-package record;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		StudentManager manager =new StudentManager();
-		
-		manager.addStudent(new Student(1,"shivu",19)); 
-		manager.addStudent(new Student(2,"karan",26));
-		manager.addStudent(new Student(3,"aujla",28));
-		
-		
-		System.out.println("studentsList");
-		manager.viewStudents();
-		
-		System.out.println("delete student");
-		manager.deleteStudent(3);
-		manager.viewStudents();
-		
-		manager.addResultToStudent(1, new Result(1, "Maths", 90));
-		manager.addResultToStudent(1, new Result(1, "Science", 85));
-		manager.viewStudents();
-		manager.sortStudentsByMarks();
-		manager.viewStudents();
-		manager.getTopper();
+    public static void main(String[] args) {
+
+        StudentManager manager = new StudentManager();
+        Scanner sc = new Scanner(System.in);
 
         manager.loadFromFile();
-        StudentManager manager1 = new StudentManager();
-        manager1.loadFromFile();  
-        manager1.viewStudents();
-        manager1.saveToFile();     
 
-	}
+        while (true) {
 
+            System.out.println("\n1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Delete Student");
+            System.out.println("4. Add Result");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter id: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter age: ");
+                    int age = sc.nextInt();
+
+                    manager.addStudent(new Student(id, name, age));
+                    break;
+
+                case 2:
+                    manager.viewStudents();
+                    break;
+
+                case 3:
+                    System.out.print("Enter id to delete: ");
+                    int delId = sc.nextInt();
+                    manager.deleteStudent(delId);
+                    break;
+
+                case 4:
+                    System.out.print("Enter student id: ");
+                    int sid = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter subject: ");
+                    String subject = sc.nextLine();
+
+                    System.out.print("Enter marks: ");
+                    int marks = sc.nextInt();
+
+                    manager.addResultToStudent(sid, new Result(sid, subject, marks));
+                    break;
+
+                case 5:
+                    manager.saveToFile();
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
 }
